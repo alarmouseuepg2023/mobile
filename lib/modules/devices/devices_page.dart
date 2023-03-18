a70f06fb17998a8a3f16c030e4ff29c9ee5a24ce
Renato Ruppel <heruppel2@gmail.com>
2023-03-18 19:42:27 -0300

    feat(devices): device pages initialized

---
+++ lib/modules/devices/devices_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class DevicesPage extends StatefulWidget {
+  const DevicesPage({super.key});
+
+  @override
+  State<DevicesPage> createState() => _DevicesPageState();
+}
+
+class _DevicesPageState extends State<DevicesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: const Text("devices"),
+    );
+  }
+}
