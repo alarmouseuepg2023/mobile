fa24e6066388ba3bd4a9e75c89bd703570cb44e1
Renato Ruppel <heruppel2@gmail.com>
2023-03-21 20:28:54 -0300

    feat(user): added user storaged check for login or home redirect

--- lib/modules/splash/splash_page.dart
+++ lib/modules/splash/splash_page.dart
@@ -1,16 +1,48 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:mobile/providers/auth/auth_provider.dart';
 import 'package:mobile/shared/themes/app_colors.dart';
 
-class SplashPage extends StatelessWidget {
+import '../../service/index.dart';
+
+class SplashPage extends ConsumerStatefulWidget {
   const SplashPage({super.key});
 
   @override
+  ConsumerState<SplashPage> createState() => _SplashPageState();
+}
+
+class _SplashPageState extends ConsumerState<SplashPage> {
+  bool user = false;
+
+  @override
+  void initState() {
+    getStorageData();
+    super.initState();
+  }
+
+  Future<void> getStorageData() async {
+    final hasUser = await ref.read(authProvider).getUserData();
+    if (hasUser) {
+      final accessToken = ref.read(authProvider).accessToken;
+      dio.options.headers[HttpHeaders.authorizationHeader] =
+          "bearer $accessToken";
+    }
+    setState(() {
+      user = hasUser;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
-    Future.delayed(const Duration(seconds: 2))
-        .then((_) => Navigator.pushReplacementNamed(
-              context,
-              '/login',
-            ));
+    Future.delayed(const Duration(seconds: 2)).then((_) {
+      Navigator.pushReplacementNamed(
+        context,
+        user ? '/home' : '/login',
+      );
+    });
 
     return Container(
       decoration: const BoxDecoration(color: AppColors.primary),
