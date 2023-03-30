31b7656410dc015d0e2a902ce81bddcd5816823c
Renato Ruppel <heruppel2@gmail.com>
2023-03-30 15:51:23 -0300

    feat(notifications): added notifications list

--- lib/modules/notifications/notifications_page.dart
+++ lib/modules/notifications/notifications_page.dart
@@ -1,4 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:mobile/modules/notifications/notifications_controller.dart';
+import 'package:mobile/shared/models/Notifications/notification_model.dart';
+
+import '../../shared/models/Response/server_response_model.dart';
+import '../../shared/themes/app_colors.dart';
+import '../../shared/widgets/snackbar/snackbar_widget.dart';
 
 class NotificationsPage extends StatefulWidget {
   const NotificationsPage({super.key});
@@ -8,10 +15,127 @@ class NotificationsPage extends StatefulWidget {
 }
 
 class _NotificationsPageState extends State<NotificationsPage> {
+  final _notificationsController = NotificationsController();
+  bool loading = false;
+  List<NotificationModel> notifications = [];
+  int totalItems = 0;
+  bool _hasMore = true;
+  int _pageNumber = 0;
+  final int _size = 10;
+  final scrollController = ScrollController();
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      getNotifications();
+      scrollController.addListener(() {
+        if (scrollController.position.maxScrollExtent ==
+            scrollController.offset) {
+          getNotifications();
+        }
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    scrollController.dispose();
+    super.dispose();
+  }
+
+  Future<void> getNotifications() async {
+    if (!mounted || loading) return;
+    try {
+      setState(() {
+        loading = true;
+      });
+
+      final res =
+          await _notificationsController.getNotifications(_pageNumber, _size);
+      if (!mounted) return;
+      setState(() {
+        notifications.addAll(res.content.items);
+        if (res.content.items.length < _size) {
+          _hasMore = false;
+        }
+        totalItems = res.content.totalItems;
+        _pageNumber++;
+      });
+    } catch (e) {
+      if (e is DioError) {
+        ServerResponse response = ServerResponse.fromJson(e.response?.data);
+
+        GlobalSnackBar.show(
+            context,
+            response.message != ""
+                ? response.message
+                : "Ocorreu um erro ao recuperar as notificações.");
+      } else {
+        GlobalSnackBar.show(
+            context, "Ocorreu um erro ao recuperar as notificações.");
+      }
+    } finally {
+      // ignore: control_flow_in_finally
+      if (!mounted) return;
+      setState(() {
+        loading = false;
+      });
+    }
+  }
+
+  Future refresh() async {
+    setState(() {
+      loading = false;
+      _hasMore = true;
+      _pageNumber = 0;
+      notifications.clear();
+    });
+
+    getNotifications();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: const Text('NOTIFCASOES'),
+    return Padding(
+      padding: const EdgeInsets.all(15),
+      child: Column(children: [
+        Expanded(
+          flex: 1,
+          child: RefreshIndicator(
+            onRefresh: refresh,
+            child: ListView.builder(
+              physics: const AlwaysScrollableScrollPhysics(),
+              controller: scrollController,
+              itemCount: notifications.length + 1,
+              itemBuilder: (context, index) {
+                if (index < notifications.length) {
+                  final notification = notifications[index];
+                  return Column(children: [
+                    Text(
+                        "${notification.device.nickname} ${notification.inviter.name}"),
+                    const SizedBox(
+                      height: 20,
+                    )
+                  ]);
+                } else {
+                  return _hasMore
+                      ? const Center(
+                          child: SizedBox(
+                            height: 30,
+                            width: 30,
+                            child: CircularProgressIndicator(
+                              color: AppColors.primary,
+                            ),
+                          ),
+                        )
+                      : const Center();
+                }
+              },
+            ),
+          ),
+        ),
+      ]),
     );
   }
 }
